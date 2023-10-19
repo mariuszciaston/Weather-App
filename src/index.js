dca8bc9c16979a7f35ca20466a43f684e22df643
Mariusz <casmar90@gmail.com>
2023-10-19 21:45:24 +0200

    Add babel with '>0.25%' target & MiniCssExtractPlugin & CssMinimizerPlugin

--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
 import './style/style.css';
 
-// import Icon from './favicon/favicon.ico';
-
 function component() {
 	const element = document.createElement('div');
 	element.textContent = 'Hello World!';
 
-
-	
-
-	
-
-	  
-		return element;		
+	return element;
 }
 
 document.body.appendChild(component());
-
-
-
-
-
-
-
-
-
-
-
-
 
 // https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2871c88944b81fbab922d47012695ba3
 
 // Example of API call:
 // https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2871c88944b81fbab922d47012695ba3
+
+
+
+// cosnole.log('I get called from print.js!');
+
+
+
+
+const obj1 = {a: 10};
+const obj2 = {b: 20};
+const obj3 = {c: 30};
+
+// ES2018
+console.log({...obj1, ...obj2, ...obj3}); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -10,23 +12,40 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
+	devtool: 'inline-source-map',
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
 			favicon: './src/favicon/favicon.ico',
 		}),
-
 		new CopyPlugin({
 			patterns: [{ from: `./src/favicon`, to: '' }],
 		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+		}),
 	],
-
 	module: {
 		rules: [
 			{
 				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', { targets: '>0.25%' }]],
+					},
+				},
 			},
 		],
+	},
+	optimization: {
+		minimizer: [new CssMinimizerPlugin()],
 	},
 };
