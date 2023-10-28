c01ec0936dd5cfac6b21c60b37518f14e0c4e63f
Mariusz <casmar90@gmail.com>
2023-10-28 12:42:14 +0200

    Major refactor

--- src/code/logic.js
+++ src/code/logic.js
@@ -1,29 +1,20 @@
 export default class Logic {
-	static addDegrees(system) {
-		switch (system) {
-			case 'metric':
-				return '°C';
-			case 'imperial':
-				return '°F';
-			default:
-				return '°C';
-		}
+	static getDegreeUnit(system) {
+		return system === 'imperial' ? '°F' : '°C';
 	}
 
-	static windSpeedConversion(system, windSpeed) {
-		const metricWindSpeed = Math.round(windSpeed * 3.6);
-
-		switch (system) {
-			case 'metric':
-				return `${metricWindSpeed} km/h`;
-			case 'imperial':
-				return `${Math.round(windSpeed)} mph`;
-			default:
-				return `${metricWindSpeed} km/h`;
-		}
+	static convertWindSpeed(system, windSpeed) {
+		const windSpeedInKmH = Math.round(windSpeed * 3.6);
+		return system === 'imperial' ? `${Math.round(windSpeed)} mph` : `${windSpeedInKmH} km/h`;
 	}
 
-	static extractData(data, system) {
+	static getDayOfWeek(epoch) {
+		const date = new Date(epoch * 1000);
+		const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		return days[date.getDay()];
+	}
+
+	static extractWeatherData(data, system) {
 		const {
 			name: city,
 			sys: { country },
@@ -32,19 +23,50 @@ export default class Logic {
 			main: { temp, feels_like: feelsLike, pressure, humidity },
 			coord: { lat, lon },
 		} = data;
-		const temperature = Math.round(temp) + this.addDegrees(system);
-		const feelsLikeTemp = Math.round(feelsLike) + this.addDegrees(system);
-		const windSpeedUnit = this.windSpeedConversion(system, windSpeed);
+		const degreeUnit = this.getDegreeUnit(system);
+		const temperature = `${Math.round(temp)}${degreeUnit}`;
+		const feelsLikeTemp = `${Math.round(feelsLike)}${degreeUnit}`;
+		const windSpeedUnit = this.convertWindSpeed(system, windSpeed);
 		const pressureUnit = `${pressure} hPa`;
 		const humidityPercent = `${humidity}%`;
 
 		return { city, country, icon, temperature, feelsLikeTemp, humidityPercent, windSpeedUnit, pressureUnit, description, weather, lat, lon };
 	}
 
-	static epochToDay(epoch) {
-		const date = new Date(epoch * 1000);
-		const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-		return days[date.getDay()];
+	static extractNextDays(data, system) {
+		const nextDays = [];
+		const currentDate = new Date();
+		currentDate.setDate(currentDate.getDate() + 1);
+
+		for (let i = 0; i < 3; i += 1) {
+			const dateStr = currentDate.toISOString().split('T')[0];
+			const dateTimeStr = `${dateStr} 12:00:00`;
+			const dayData = data.list.find((item) => item.dt_txt === dateTimeStr);
+			if (dayData) {
+				nextDays.push({
+					day: this.getDayOfWeek(dayData.dt),
+					temp: Math.round(dayData.main.temp) + this.getDegreeUnit(system),
+					icon: dayData.weather[0].icon,
+					description: dayData.weather[0].description,
+				});
+			}
+			currentDate.setDate(currentDate.getDate() + 1);
+		}
+		return nextDays;
+	}
+
+	static async grabDataByPosition(system, lat, lon) {
+		const api = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=2871c88944b81fbab922d47012695ba3`;
+
+		try {
+			const response = await fetch(api, { mode: 'cors' });
+			if (!response.ok) throw new Error(`Localization not found`);
+			const city = (await response.json())[0].name;
+			return this.grabDataByCity(system, city);
+		} catch (error) {
+			alert(error);
+			return null;
+		}
 	}
 
 	static async grabDataByCity(system, city) {
@@ -52,9 +74,9 @@ export default class Logic {
 
 		try {
 			const response = await new Promise((resolve, reject) => {
-				setTimeout(async () => {
+				setTimeout(() => {
 					try {
-						const res = await fetch(api, { mode: 'cors' });
+						const res = fetch(api, { mode: 'cors' });
 						resolve(res);
 					} catch (error) {
 						reject(error);
@@ -63,28 +85,9 @@ export default class Logic {
 			});
 
 			if (!response.ok) throw new Error(`City '${city}' not found`);
-			const data = this.extractData(await response.json(), system);
-
+			const data = this.extractWeatherData(await response.json(), system);
 			const nextDays = await this.grabDataNextDays(system, data.lat, data.lon);
 			data.nextDays = nextDays;
-
-			return data;
-		} catch (error) {
-			alert(error);
-			return null;
-		}
-	}
-
-	static async grabDataByPosition(system, lat, lon) {
-		const api = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=2871c88944b81fbab922d47012695ba3`;
-
-		try {
-			const response = await fetch(api, { mode: 'cors' });
-
-			if (!response.ok) throw new Error(`Localization not found`);
-			let city = await response.json();
-			city = city[0].name;
-			const data = await this.grabDataByCity(system, city);
 			return data;
 		} catch (error) {
 			alert(error);
@@ -99,29 +102,7 @@ export default class Logic {
 			const response = await fetch(api, { mode: 'cors' });
 			if (!response.ok) throw new Error(`Localization not found`);
 			const data = await response.json();
-
-			const nextDays = [];
-			const currentDate = new Date();
-			currentDate.setDate(currentDate.getDate() + 1);
-
-			for (let i = 0; i < 3; i += 1) {
-				const dateStr = currentDate.toISOString().split('T')[0];
-				const dateTimeStr = `${dateStr} 12:00:00`;
-				const dayData = data.list.find((item) => item.dt_txt === dateTimeStr);
-				if (dayData) {
-					nextDays.push({
-						day: this.epochToDay(dayData.dt),
-						temp: Math.round(dayData.main.temp) + this.addDegrees(system),
-						icon: dayData.weather[0].icon,
-						description: dayData.weather[0].description,
-
-
-						
-					});
-				}
-				currentDate.setDate(currentDate.getDate() + 1);
-			}
-
+			const nextDays = this.extractNextDays(data, system);
 			return nextDays;
 		} catch (error) {
 			alert(error);
